2eb209d73fa178973b195e00301a949bd1c8e347
Assassin Nima <88039431+n-ii-ma@users.noreply.github.com>
2022-05-10 17:50:20 +0430

    Banner for Mobile

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,13 @@
-import Box from "@mui/material/Box";
-
 const Header = () => {
   return (
-    <Box>
-      <img
-        src="https://res.cloudinary.com/de3kst1ud/image/upload/v1652022389/Header/header_mobile_hmsq4f.jpg"
-        alt="Galaxy Note 20 Ultra"
-        className="mobileImage"
-      />
-      <h1 className="overlay-header">A Galaxy in the Palm of Your Hand</h1>
-      <p className="overlay-p">
-        Buy Your Favorite Samsung Galaxy Note on Note Land
-      </p>
-    </Box>
+    <section className="top-banner">
+      <article>
+        <h1>A Galaxy in the Palm of Your Hand</h1>
+        <div className="paragraph-container">
+          <p>Buy Your Favorite Samsung Galaxy Note on Note Land</p>
+        </div>
+      </article>
+    </section>
   );
 };
 
