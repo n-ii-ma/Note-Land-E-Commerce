fd6127d295e0cad7232bfafba28f644cbce4538f
Assassin Nima <88039431+n-ii-ma@users.noreply.github.com>
2022-05-10 20:37:19 +0430

    Banner CSS

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,7 @@ const Header = () => {
     <section className="top-banner">
       <article>
         <h1>A Galaxy in the Palm of Your Hand</h1>
-        <div className="paragraph-container">
-          <p>Buy Your Favorite Samsung Galaxy Note on Note Land</p>
-        </div>
+        <p>Buy Your Favorite Samsung Galaxy Note on Note Land</p>
       </article>
     </section>
   );
