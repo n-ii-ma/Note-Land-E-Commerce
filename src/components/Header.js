f46c790c0aba22ba88dd9444b6e266bcca79cfc3
Assassin Nima <88039431+n-ii-ma@users.noreply.github.com>
2022-05-11 11:08:47 +0430

    CSS landscape

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,24 @@ const Header = () => {
   return (
     <section className="top-banner">
       <img
-        src="https://res.cloudinary.com/de3kst1ud/image/upload/v1652022389/Header/header_mobile_hmsq4f.jpg"
+        src="https://res.cloudinary.com/de3kst1ud/image/upload/v1652022389/Header/header_portrait_hmsq4f.jpg"
         alt="Samsung Galaxy Note 20 Ultra"
+        className="portrait"
+      />
+      <img
+        src="https://res.cloudinary.com/de3kst1ud/image/upload/v1652248828/Header/header_landscape_lwzaaj.jpg"
+        alt="Samsung Galaxy Note 20 Ultra"
+        className="landscape"
       />
       <article>
         <h1>A Galaxy in the Palm of Your Hand</h1>
-        <p>Buy Your Favorite Samsung Galaxy Note on Note Land</p>
+        <p className="text-portrait">
+          Buy Your Favorite Samsung Galaxy Note on Note Land
+        </p>
       </article>
+      <p className="text-landscape">
+        Buy Your Favorite Samsung Galaxy Note on Note Land
+      </p>
     </section>
   );
 };
