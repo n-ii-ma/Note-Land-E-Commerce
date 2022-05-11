775aa60b8cc771f6b82894672266da44d287f252
Assassin Nima <88039431+n-ii-ma@users.noreply.github.com>
2022-05-11 11:15:42 +0430

    CSS Update

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,12 +4,12 @@ const Header = () => {
       <img
         src="https://res.cloudinary.com/de3kst1ud/image/upload/v1652022389/Header/header_portrait_hmsq4f.jpg"
         alt="Samsung Galaxy Note 20 Ultra"
-        className="portrait"
+        className="img-portrait"
       />
       <img
         src="https://res.cloudinary.com/de3kst1ud/image/upload/v1652248828/Header/header_landscape_lwzaaj.jpg"
         alt="Samsung Galaxy Note 20 Ultra"
-        className="landscape"
+        className="img-landscape"
       />
       <article>
         <h1>A Galaxy in the Palm of Your Hand</h1>
