bc5ce66295e689ef9400d43f67e6c0a38cb53071
Assassin Nima <88039431+n-ii-ma@users.noreply.github.com>
2022-05-11 10:06:40 +0430

    Added Grid (CSS Protrait)

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,10 @@
 const Header = () => {
   return (
     <section className="top-banner">
+      <img
+        src="https://res.cloudinary.com/de3kst1ud/image/upload/v1652022389/Header/header_mobile_hmsq4f.jpg"
+        alt="Samsung Galaxy Note 20 Ultra"
+      />
       <article>
         <h1>A Galaxy in the Palm of Your Hand</h1>
         <p>Buy Your Favorite Samsung Galaxy Note on Note Land</p>
