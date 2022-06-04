3341d21c4541d97f87b488ce7ce18045b9aa3beb
Assassin Nima <88039431+n-ii-ma@users.noreply.github.com>
2022-06-04 17:32:50 +0430

    First clear the cart and then log out the user

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -96,11 +96,11 @@ const NavBar = () => {
       navigate("/", { replace: true });
     }
 
-    // Logout user
-    dispatch(logoutUser());
-
     // Clear cart
     dispatch(clearCart());
+
+    // Logout user
+    dispatch(logoutUser());
 
     // Purge redux-persist state 1 second after logout
     setTimeout(() => persistor.purge(), 1000);
