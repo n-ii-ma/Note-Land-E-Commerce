e508e34fbfe4aab3b4f0b4ea94c5b8fd17346155
Nima <88039431+n-ii-ma@users.noreply.github.com>
2023-05-02 13:15:13 +0330

    Change production api address

--- client/src/config/axiosConfig.js
+++ client/src/config/axiosConfig.js
@@ -4,14 +4,14 @@ const isProduction = process.env.NODE_ENV === "production";
 
 export const axiosApiPrivate = axios.create({
   baseURL: isProduction
-    ? "https://e-commerce-pern.herokuapp.com/api/v1"
+    ? "https://e-commerce-pern.cyclic.app/api/v1"
     : "http://localhost:3000/api/v1",
   withCredentials: true,
 });
 
 export const axiosApiPublic = axios.create({
   baseURL: isProduction
-    ? "https://e-commerce-pern.herokuapp.com/api/v1"
+    ? "https://e-commerce-pern.cyclic.app/api/v1"
     : "http://localhost:3000/api/v1",
   withCredentials: false,
 });
